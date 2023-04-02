be09052dcf39db45ed9361a1a49108d917001653
RoysManfo <manforoys@gmail.com>
2023-04-02 22:37:07 +0200

    Modified login and validation sequence

--- src/main.py
+++ src/main.py
@@ -1,23 +1,31 @@
 from user import User
-from secure import Secure
+from cryptography.fernet import Fernet
+import getpass
+import base64
 
-def generate_password_master(USER: User, SECURE: Secure) -> str:
+
+def generate_key(USER: User) -> bytes:
     print("""
 In order to secure your password at best, you have to create a Password Master
 
-A Password Master (PM) is a password you will have to remember and make shure to remember it
-as we won't be able to restore or change it if forgotten, for security reasons.   
-    
-A Password Master (PM) must be at least 8 characters long and contain at least 1 uppercase letter
+A Password Master (PM) is a password you will have to remember and make sure to remember it
+as we won't be able to restore or change it if forgotten, for security reasons.
+
+A Password Master (PM) must be at least 8 characters long (max 32) and contain at least 1 uppercase letter
     """)
     passw: str = input("Your Password Manager:  ")
 
-    while not USER.check_password(passw):
+    while not USER.check_password(passw) or len(passw) > 32:
         print("Your password doesn't satisfy some requirements, type another one")
         passw: str = input("Your Password Manager:  ")
 
-    USER.password_master = passw
-    pass_hash: bytes = SECURE.encrypt(passw)
+    for _ in range(len(passw), 32):
+        passw += "="
+
+    USER.create_vault(passw)
+    fernet = Fernet(base64.urlsafe_b64encode(passw.encode()))
+    key = fernet.generate_key()
+    return key
 
 
 def main():
@@ -25,7 +33,21 @@ def main():
     ### Point of start of the program
     """
     USER = User()
-    SECURE = Secure(USER.key)
 
-    if USER.password_master == "":
-        generate_password_master(USER, SECURE)
+    if USER.password_manager == b"":
+        USER.key = generate_key(USER)
+
+    else:
+        pm = getpass.getpass("Enter password manager (invisible):  ")
+        while not USER.validate_key(pm):
+            print("Not the correct password")
+            pm = getpass.getpass("Enter password manager (invisible):  ")
+        for _ in range(len(pm), 32):
+            pm += "="
+        fernet = Fernet(base64.urlsafe_b64encode(pm.encode()))
+        USER.key = fernet.generate_key()
+
+    print("Logged sucessfully")
+
+
+main()
--- src/user.py
+++ src/user.py
@@ -1,17 +1,33 @@
+import os
+import json
+import hashlib
+
 
 class User(object):
-    def __init__(self, password_master: str = ""):
-        self.password_master = password_master
-        self.logged = self.is_logged()
+    def __init__(self):
+        self.password_manager = self.get_PM()
         self.key = ""
 
+    def get_PM(self) -> bytes:
+        try:
+            with open(os.path.join(os.path.dirname(__file__), "data", "vault.json"), "r") as f:
+                file = json.load(f)
+                return str(file['PM-hash']).encode()
 
-    def is_logged() -> bool:
-        pass
+        except FileNotFoundError:
+            return b""
 
-    def generate_password_master(self):
-        with open("./data/password_master.key", "w") as password:
-            password.write(self.password_master)
+        except json.JSONDecodeError:
+            return b""
+
+        except KeyError:
+            return b""
+
+    def validate_key(self, key: str) -> bool:
+        for _ in range(len(key), 32):
+            key += "="
+        key = hashlib.sha512(key.encode()).hexdigest()
+        return True if key == self.password_manager.decode() else False
 
     def check_password(self, passw: str) -> bool:
         """
@@ -28,6 +44,13 @@ class User(object):
 
         if passw.upper() == passw or passw.lower() == passw:
             return False
-            
-        return True 
 
+        return True
+
+    def create_vault(self, password):
+        os.makedirs(os.path.join(os.path.dirname(
+            __file__), "data"), exist_ok=True)
+        with open(os.path.join(os.path.dirname(__file__), "data", "vault.json"), "w") as f:
+            context = {
+                "PM-hash": hashlib.sha512(password.encode()).hexdigest()}
+            json.dump(context, f, indent=4, sort_keys=True)
