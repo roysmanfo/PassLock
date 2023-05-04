95533e8f3f6ea17691eb147c5f6b2d1b1e2fac3a
RoysManfo <manforoys@gmail.com>
2023-05-04 22:33:23 +0200

    Start implementing first commands

--- src/main.py
+++ src/main.py
@@ -3,6 +3,21 @@ from cryptography.fernet import Fernet
 import getpass
 import base64
 import sys
+import json
+import os
+
+
+class Color:
+    RED = "\033[0;31m"
+    GREEN = "\033[0;32m"
+    BLUE = "\033[0;34m"
+    CYAN = "\033[0;36m"
+    YELLOW = "\033[1;33m"
+    PURPLE = "\033[0;35m"
+    RESET = "\033[0m"
+
+
+col = Color
 
 
 def generate_key(USER: User) -> bytes:
@@ -24,9 +39,10 @@ A Password Master (PM) must be at least 8 characters long (max 32) and contain a
         passw: str = getpass.getpass("Your Password Manager:  ")
 
         if not USER.check_password(passw) or len(passw) > 32:
-            print("Your password doesn't satisfy some requirements, type another one")
+            print(
+                f"{col.RED}Your password doesn't satisfy some requirements, type another one{col.RESET}")
             passw: str = getpass.getpass("Your Password Manager:  ")
-        
+
         else:
 
             passw2 = getpass.getpass("Your Password Manager (again):  ")
@@ -34,7 +50,8 @@ A Password Master (PM) must be at least 8 characters long (max 32) and contain a
             if passw == passw2:
                 approved = True
             else:
-                print("Passwords do not match, insert again the password")
+                print(
+                    f"{col.RED}Passwords do not match, insert again the password{col.RESET}")
 
     # Create padding
     for _ in range(len(passw), 32):
@@ -54,7 +71,7 @@ def login(user: User) -> bytes:
     try:
         pm = getpass.getpass("Enter password manager (invisible):  ")
         while not user.validate_key(pm):
-            print("Not the correct password")
+            print(f"{col.RED}Not the correct password{col.RESET}")
             pm = getpass.getpass("Enter password manager (invisible):  ")
 
     except KeyboardInterrupt:
@@ -75,18 +92,75 @@ def main():
     USER = User()
     USER.key = generate_key(
         USER) if USER.password_manager == b"" else login(USER)
-    print("Logged sucessfully")
+    print(f"{col.GREEN}Logged sucessfully{col.RESET}")
     while True:
         try:
-            command = input('PassLock> ')
-            command = command.strip().lower().split(' ')
+            print(f"{col.BLUE}PassLock> {col.RESET}", end='')
+            command = input()
+            command = command.strip().split(' ')
             while command.count('') > 0:
                 command.remove('')
-            
-            print(command)
+            command[0].lower()
+            run_command(command, USER.key)
 
+            print(command)
         except KeyboardInterrupt:
-            sys.exit(0)
+            break
+
+    sys.exit(0)
+
+
+def run_command(args: list, key: bytes):
+    """
+    # Examples of outputs from these commmands
+    ## `list`
+
+    GitHub: 2 fields
+    Google: 3 fields
+
+    ## `get VALUE` case sensitive
+    
+    GitHub
+    ================================================================
+    Username: Github1234
+    Password: mypassword123
+    ================================================================
+    """
+
+    fernet = Fernet(key)
+
+    if args[0] == 'exit':
+        sys.exit(0)
+
+    elif args[0] == 'list':
+        with open(os.path.join('data', 'vault.json'), 'r') as f:
+            apps: dict = json.load(f)['Apps']
+            if len(apps) == 0:
+                print(f'{col.YELLOW}No apps registered yet{col.RESET}')
+                return
+
+            for app in apps.keys():
+                print(
+                    f'{app}: {col.CYAN}{len(apps[app].keys())}{col.RESET} fields')
+
+    elif args[0] == 'get':
+        if len(args) < 2:
+            print(f'{col.RED}No app name specified{col.RESET}')
+        elif len(args) > 2:
+            print(f'{col.RED}Too many arguments specified{col.RESET}')
+
+        else:
+            with open(os.path.join('data', 'vault.json'), 'r') as f:
+                apps: dict = json.load(f)['Apps']
+                if args[1] not in apps.keys():
+                    print(f'\n{col.RED}App not found{col.RESET}')
+                else:
+                    print(f'{col.CYAN}{args[1].upper()}{col.RESET}')
+                    print('================================================================')
+                    for _, val in enumerate(apps[args[1]].keys()):
+                        print(f'{val}: {fernet.decrypt(apps[args[1]][val])}')
+                    print('================================================================')
+                
 
 if __name__ == '__main__':
     main()
