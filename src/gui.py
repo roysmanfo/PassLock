e44d0d30fc9f8aaca46af9bc859c9230cd35f5f0
RoysManfo <manforoys@gmail.com>
2024-05-21 20:15:59 +0200

    Reorganize project

--- src/gui.py
+++ src/gui.py
@@ -1,5 +1,12 @@
+from enum import IntEnum, auto
 import tkinter as tk
+from typing import Callable
 from colors import Colors
+
+
+class PageEnum(IntEnum):
+    LOGIN = auto()
+    MAIN  = auto()
 
 def get_geometry() -> str:
     """
@@ -18,30 +25,59 @@ def get_geometry() -> str:
 def set_colors():
     root.configure(bg=Colors.PL_GRAY)    
 
-def validate_password(_):
-    print(pm.get())
+
+def setup_window():
+    set_colors()
+    root.geometry(get_geometry())
+    root.title("PassLock")
+    root.resizable(False, False)
+
+class Pages:
+    current_page: PageEnum
+
+    @staticmethod
+    def goto_page(page: PageEnum, *args, **kwargs) -> None:
+        match page:
+            case PageEnum.LOGIN: Pages.login_page(*args, *kwargs)
+            case PageEnum.MAIN: Pages.main_page(*args, *kwargs)
+            
+            # for now use this as 404, even if this case should not happen
+            case _: Pages.login_page(*args, *kwargs)
+
+    @staticmethod
+    def login_page(validate_password: Callable[[str], bool]):
+
+        def getpasswd(_):
+            passwd = pm.get()
+            if validate_password(passwd):
+                login_frame.destroy()
+                Pages.goto_page(PageEnum.MAIN)
+
+        Pages.current_page = PageEnum.LOGIN
+
+
+        # login_frame
+        login_frame = tk.Frame(root, bg=Colors.PL_GRAY, border=0)
+        login_frame.place(relx=.5, rely=.5, anchor=tk.CENTER)
+        login_frame.pack(fill="both", expand=True, pady=200)
+
+        # labels
+        tk.Label(login_frame, text="PassLock", bg=Colors.PL_GRAY, fg=Colors.PL_PURPLE, font=('Inter', '70', 'bold'), height=1).pack()
+        tk.Label(login_frame, text="Enter password manager", bg=Colors.PL_GRAY, fg="#eee", font=('Inter 16 bold'), height=5).pack()
+
+        # login field
+        pm = tk.StringVar()
+        login_field = tk.Entry(login_frame, font=('monospace 20'), width=35, bg=Colors.PL_LGRAY, border=0, borderwidth=0, fg=Colors.PL_LIGHT, show="•", justify="center", textvariable=pm)
+        login_field.bind("<Return>", func=getpasswd)
+        login_field.pack()
+
+    @staticmethod
+    def main_page():
+        Pages.current_page = PageEnum.LOGIN
+        tk.Label(root, text="Main Page", bg=Colors.PL_GRAY, fg=Colors.PL_PURPLE, font=('Inter', '70', 'bold'), height=1).pack()
 
 
 
 root = tk.Tk()
-set_colors()
-root.geometry(get_geometry())
-root.title("PassLock")
-root.resizable(False, False)
-
-
-# login_frame
-login_frame = tk.Frame(root, bg=Colors.PL_GRAY, border=0)
-login_frame.place(relx=.5, rely=.5, anchor=tk.CENTER)
-login_frame.pack(fill="both", expand=True, pady=200 )
-
-# labels
-tk.Label(login_frame, text="PassLock", bg=Colors.PL_GRAY, fg=Colors.PL_PURPLE, font=('Inter', '70', 'bold'), height=1).pack()
-tk.Label(login_frame, text="Enter password manager", bg=Colors.PL_GRAY, fg="#eee", font=('Inter 16 bold'), height=5).pack()
-
-# login field
-pm = tk.StringVar()
-login_field = tk.Entry(login_frame, font=('monospace 20'), width=35, bg=Colors.PL_LGRAY, border=0, borderwidth=0, fg=Colors.PL_LIGHT, show="•", justify="center", textvariable=pm)
-login_field.bind("<Return>", func=validate_password)
-login_field.pack()
-
+page: Pages = Pages()
+setup_window()
--- src/main.py
+++ src/main.py
@@ -3,25 +3,14 @@ import auth
 from vault import vault
 
 
-
-def main():
-    """
-    ### Point of start of the program
-    """
-    # USER = User(vault.path)
-    # USER.key = login.generate_key(USER) if USER.password_manager == b"" else login.login(USER)
-    
+if __name__ == '__main__':
 
     if vault.empty:
         vault.key = auth.register(vault, "PassLock")
 
-        print(vault.key)
-
+    gui.page.login_page(auth.validate_password)
     gui.root.mainloop()
 
-
-if __name__ == '__main__':
-    main()
     try:
         vault.connection.close()
     except:
