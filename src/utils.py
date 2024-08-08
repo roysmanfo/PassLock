1798a56cd06f8b56d485cb70435623d7e50cb564
roysmanfo <manforoys@gmail.com>
2024-08-08 09:59:21 +0200

    add typing

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,13 @@
 import base64
 import json
+
+from typing import Optional
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
 
 from user import User
 
-def update_vault(user: User, pm_hash: str = None, hint: str = None, apps: dict = None):
+def update_vault(user: User, pm_hash: Optional[str] = None, hint: Optional[str] = None, apps: Optional[dict] = None):
     with open(user.vault_path, 'r') as f:
         file = json.load(f)
         pm_hash: str = file['PM-hash'] if pm_hash is None else pm_hash
