890ce30d559ced303d398018823c049d831ca71a
roysmanfo <manforoys@gmail.com>
2024-09-05 22:47:49 +0200

    use `conf.py` to get the version

--- setup.py
+++ setup.py
@@ -1,23 +1,20 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-CONF = {}
+from src.conf import VERSION
+
 DIR = Path(__file__).parent
-with open(str(DIR / 'src' / '.conf'), 'r') as fp:
-    lines = fp.read().splitlines()
-    # if additional info is added in .conf the logic should change
-    CONF['version'] = lines[0][len('version='):]
 
 with open(str(DIR / 'requirements.txt'), 'r') as fp:
     lines = fp.read().splitlines()
-    CONF['requirements'] = lines if lines else []
+    REQUIREMENTS = lines if lines else []
 
 setup(
     name='passlock',
-    version=CONF['version'],
+    version=VERSION,
     author='roysmanfo',
     url='https://github.com/roysmanfo/PassLock',
-    install_requires=CONF['requirements'],
+    install_requires=REQUIREMENTS,
     packages=find_packages(include=['src']),
     entry_points={
         'console_scripts': ['passlock=src.main:main']
