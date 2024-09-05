1167c7b3bfdb1afe54374489446db5d00d6e6ce4
roysmanfo <manforoys@gmail.com>
2024-09-05 22:54:24 +0200

    add SECURE_STORAGE_PATH

--- src/conf.py
+++ src/conf.py
@@ -2,7 +2,7 @@ import os
 import configparser
 
 
-def _get_conf():
+def __get_conf__():
     c = configparser.ConfigParser()
     c.read(CONF_PATH)
     return c
@@ -12,14 +12,17 @@ def _get_conf():
 BIN_PATH = os.path.dirname(os.path.realpath(__file__))
 
 # the location of the vault
-VAULT_PATH = os.path.join(os.path.expanduser("~"),
-                          ".passlock", "vault.json")
+VAULT_PATH = os.path.join(os.path.expanduser("~"), ".passlock", "vault.json")
+
+# the location of the secure storage
+SECURE_STORAGE_PATH = os.path.join(
+    os.path.expanduser("~"), ".passlock", "secure_storage")
 
 # the location of the configuration file
 CONF_PATH = os.path.join(BIN_PATH, ".conf")
 
 # the actual configurations as an object (ConfigParser)
-CONF = _get_conf()
+CONF = __get_conf__()
 
 # the curent version
 VERSION = CONF['general'].get("version", None)
